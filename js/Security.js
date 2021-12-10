30b1fccf44348e1e5d773a94d8713ea79af4b98c
reesha1806 <84621535+reesha1806@users.noreply.github.com>
2021-12-10 17:57:04 +0530

    Update Security.js

--- js/Security.js
+++ js/Security.js
@@ -2,7 +2,7 @@ class Security {
 
     constructor(){
 
-        this.access1 = createInput("Code1")
+        this.access1 = createInput("").attribute("placeholder", "code1");
         this.access1.position(100,90);
         this.access1.style('background', 'white');  
 
@@ -10,7 +10,7 @@ class Security {
         this.button1.position(100,120);
         this.button1.style('background', 'lightgrey');    
 
-        this.access2 = createInput("Code1")
+        this.access2 = createInput("").attribute("placeholder", "code2");
         this.access2.position(700,190);
         this.access2.style('background', 'white');  
 
@@ -18,7 +18,7 @@ class Security {
         this.button2.position(700,220);
         this.button2.style('background', 'lightgrey');
 
-        this.access3 = createInput("Code1")
+        this.access3 = createInput("").attribute("placeholder", "code3");
         this.access3.position(100,290);
         this.access3.style('background', 'white');  
 
